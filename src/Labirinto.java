fdb4bed5a9c73068c2ed89b6eb0b4529b88b94ad
Lucas Vargas <lucasvargasjob@gmail.com>
2017-05-29 13:35:40 -0300

    Criando projeto.
    Classe de Graphics extendida.

---
+++ src/Labirinto.java
@@ -0,0 +1,27 @@
+import com.senac.SimpleJava.Console;
+import com.senac.SimpleJava.Graphics.Canvas;
+import com.senac.SimpleJava.Graphics.GraphicApplication;
+
+public class Labirinto extends GraphicApplication {
+
+	@Override
+	protected void draw(Canvas arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void loop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void setup() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+
+}
