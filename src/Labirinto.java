cc4a8eb286a07cac409d24491b511223ff0a62ce
Lucas Vargas <lucasvargasjob@gmail.com>
2017-05-29 17:52:23 -0300

    Realização da APS 2 - Carregamento do arquivo.txt

--- src/Labirinto.java
+++ src/Labirinto.java
@@ -1,3 +1,7 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+import com.senac.SimpleJava.Console;
 import com.senac.SimpleJava.Graphics.Canvas;
 import com.senac.SimpleJava.Graphics.GraphicApplication;
 import com.senac.SimpleJava.Graphics.Resolution;
@@ -15,6 +19,7 @@ public class Labirinto extends GraphicApplication {
 		Resolution res = Resolution.HIGHRES;
 		this.setResolution(res);
 		this.setFramesPerSecond(30);
+		carregaArquivo();
 		
 		
 	}
@@ -26,6 +31,87 @@ public class Labirinto extends GraphicApplication {
 	}
 
 
+	
+	//METODOS -----------------------------------------------------------------------------------------------------------
+	
+	public void carregaArquivo (){
+		Room sala[] = new Room [31];
+		String[] linha = new String[31];
+		Scanner scanner = null;
+		int i = 0;
+		double room=0, east=0, west=0, north=0, south=0, down=0, up=0;
+		
+		try {
+			scanner = new Scanner (new FileReader("Labirinto.txt")).useDelimiter("\n");
+		}catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		while (scanner.hasNext()) {
+			linha[i] = scanner.next();
+			String aux[] = linha[i].split(";");
+			for (int b=0; b < aux.length ; b ++) {
+				
+				char [] letras = aux[b].toCharArray();
+				
+				if (letras[0] == 'r'){	
+					aux[b] = aux[b].replace("room ", "");
+					room = Double.parseDouble(aux[b]);	
+				}
+				
+				if (letras[0] == 'e'){	
+					aux[b] = aux[b].replace("east ", "");
+					east = Double.parseDouble(aux[b]);	
+				}
+					
+				if (letras[0] == 'w'){	
+					aux[b] = aux[b].replace("west ", "");
+					west = Double.parseDouble(aux[b]);	
+				}
+					
+				if (letras[0] == 'n'){	
+					aux[b] = aux[b].replace("north ", "");
+					north = Double.parseDouble(aux[b]);	
+				}
+					
+				if (letras[0] == 's'){	
+					aux[b] = aux[b].replace("south ", "");
+					south = Double.parseDouble(aux[b]);	
+				}
+					
+				if (letras[0] == 'd'){	
+					aux[b] = aux[b].replace("down ", "");
+					down = Double.parseDouble(aux[b]);	
+				}
+				
+				if (letras[0] == 'u'){	
+					aux[b] = aux[b].replace("up ", "");
+					up = Double.parseDouble(aux[b]);	
+				}
+					
+			}
+			sala[i] = new Room (room, east, west, north,  south, down, up);
+			i++;			
+		}
+				
+
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 
 }
---
+++ src/Room.java
@@ -0,0 +1,37 @@
+
+public class Room {
+	
+	double room = 0;
+	double east = 0;
+	double west = 0;
+	double north = 0;
+	double south = 0;
+	double down = 0;
+	double up = 0;
+	
+	public Room ( double room, double east, double west, double north, double south, double down, double up){
+		this.room = room;
+		this.east = east;
+		this.west = west;
+		this.north = north;
+		this.south = south;
+		this.down = down;
+		this.up = up;
+	}
+	
+	
+	public String toString(){
+		return "Sala "+room+" "+
+				"south: " + south+ " "+
+				"east: "+east+ " "+
+				"west: "+ west+ " "+
+				"north: "+north+ " "+
+				"south: "+south+ " "+
+				"down: "+ down+ " "+
+				"up: "+ up + " ";
+		
+	}
+	
+	
+
+}
